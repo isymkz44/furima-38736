b4086b76a0356e5f3b8fcfe724577adbf348dffc
isymkz44 <falot.323.66@yahoo.ne.jp>
2022-12-27 16:50:38 +0900

    新規登録機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,15 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  before_action :basic_auth, :configure_permitted_parameters, if: :devise_controller?  #もしdeviseに関するコントローラーの処理であれば、そのときだけconfigure_permitted_parametersメソッドを実行するように設定
+ 
 
+  private   #Before_actionで記述するもの（特にApplicationController内に記述するもの）はストロングパタメーター、即ちprivate以下に記述することになる
+    def basic_auth
+        authenticate_or_request_with_http_basic do |username, password|
+        username == 'admin' && password == '2222'
+        end
+    end 
 
-  private
-
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == 'admin' && password == '2222'
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :first_name_reading, :last_name_reading, :nickname, :email, :birthday, :encrypted_password])
     end
-  end
 end
