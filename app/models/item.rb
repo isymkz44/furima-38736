684a2f6e275a71d3610f0a5f51d0543e157ad272
isymkz44 <falot.323.66@yahoo.ne.jp>
2022-12-30 17:45:55 +0900

    itemsモデル　バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+
+  validates :name,                presence: true
+  validates :description,         presence: true
+  validates :price,               presence: true
+  validates :category_id,         presence: true
+  validates :condition_id,        presence: true
+  validates :shipping_date_id,    presence: true
+  validates :shipping_charge_id,  presence: true
+  validates :prefecture_id,       presence: true
+  validates :user,                presence: true
+
+  belongs_to :user
+  has_one :buy
 end
