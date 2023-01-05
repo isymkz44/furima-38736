607ea772e48bc3099fd59d23bd7b16eb0ec15129
isymkz44 <falot.323.66@yahoo.ne.jp>
2023-01-05 15:58:53 +0900

    itemsコントローラーcreate/create_paeramsアクションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,18 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.new(create_params)
+    if @item.save
+      redirect_to action: :index
+    else
+      render :new
+    end
+  end
+
+   private
+   def create_params
+     params.require(:item).permit(:name,:description,:image,:price,:category_id,:condition_id,:shipping_charge_id,:shipping_date_id,:prefecture_id).merge(user_id: current_user.id)
+   end
 end
