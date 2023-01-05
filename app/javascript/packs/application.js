533f86766ada39c8438fc179508443a26b852406
isymkz44 <falot.323.66@yahoo.ne.jp>
2023-01-05 15:57:30 +0900

    turbolinksの削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+// require("turbolinks").start() ←HTMLファイルだけの読み込みにならないように(jsファイルも読み込まれるように)、削除
 require("@rails/activestorage").start()
 require("channels")
 
