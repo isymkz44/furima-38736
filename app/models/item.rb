de61921fe9e11cdb1bc96e054f5b27bc26e98cb5
isymkz44 <falot.323.66@yahoo.ne.jp>
2023-01-05 16:07:19 +0900

    【fix】バリデーションにかける、ActiveHashのIDの修正/モデル名をキャメルケースへ修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,24 +12,26 @@ class Item < ApplicationRecord
             :shipping_charge_id,
             :shipping_date_id,     presence: true
   
-            validates :category_id,
+  validates :category_id,
             :condition_id,
             :prefecture_id,
             :shipping_charge_id,
-            :shipping_date_id,     numericality: { other_than: 1,message: "can't be blank" } 
+            :shipping_date_id,     numericality: { other_than: 0,message: "can't be blank" } 
 
   validates :name,                presence: true
   validates :description,         presence: true
   validates :price,               presence: true
+                                  #,numericality: { grater_than: 300, less_than_or_equal: 9999999 }
+                                  #,format: { /\A[0-9]+\z/ }
   validates :category_id,         presence: true
   validates :condition_id,        presence: true
   validates :shipping_date_id,    presence: true
   validates :shipping_charge_id,  presence: true
   validates :prefecture_id,       presence: true
-  validates :user,                presence: true
+  # validates :user,                presence: true 
 
   belongs_to :user
-  has_one :buy
+  # has_one :buy
   has_one_attached :image #複数イメージを保存する？としたら違うメソッドに
 
   
