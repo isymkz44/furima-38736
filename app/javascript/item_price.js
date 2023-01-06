6526bf4597030e5d100ed00290632ddff96acc6f
isymkz44 <falot.323.66@yahoo.ne.jp>
2023-01-06 21:25:10 +0900

    【fix】小数点切り捨てへの記述変更

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,13 +1,10 @@
 function calcPrice() {
   const priceInput = document.getElementById("item-price");
-  // console.log(priceInput)
    priceInput.addEventListener("input", () => {
      const addTaxDom = document.getElementById("add-tax-price");
-      //  console.log(priceInput.innerHTML);
      addTaxDom.innerHTML = Math.round(priceInput.value * 0.1 );
      const addProfitDom = document.getElementById("profit");
-      //  console.log(priceInput.innerHTML);
-     addProfitDom.innerHTML = Math.round(priceInput.value - Math.round(priceInput.value * 0.1 ))
+     addProfitDom.innerHTML = Math.round(priceInput.value - Math.floor(priceInput.value * 0.1 ))
  })
 };
 
