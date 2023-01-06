efa8322f65cc45d020d612c9353269114aeba54a
isymkz44 <falot.323.66@yahoo.ne.jp>
2023-01-06 19:41:15 +0900

    画像表示のバリデーションの追加・値段のバリテーション設定・不要な記述の削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,29 +10,29 @@ class Item < ApplicationRecord
             :condition_id,
             :prefecture_id,
             :shipping_charge_id,
-            :shipping_date_id,     presence: true
+            :shipping_date_id,
+            :image,               presence: true
   
   validates :category_id,
             :condition_id,
             :prefecture_id,
             :shipping_charge_id,
-            :shipping_date_id,     numericality: { other_than: 0,message: "can't be blank" } 
+            :shipping_date_id,    numericality: { other_than: 0,message: "can't be blank" } 
 
   validates :name,                presence: true
   validates :description,         presence: true
-  validates :price,               presence: true
-                                  #,numericality: { grater_than: 300, less_than_or_equal: 9999999 }
-                                  #,format: { /\A[0-9]+\z/ }
+  validates :price,               presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message:"is invalid" },
+                                  # numericality: { in : 300..9999999 } #←inオプションを使用するとこんな感じ？
+                                  format: { with: /\A[0-9]+\z/, message: "is invalid. Input half-width numbers" }
   validates :category_id,         presence: true
   validates :condition_id,        presence: true
   validates :shipping_date_id,    presence: true
   validates :shipping_charge_id,  presence: true
   validates :prefecture_id,       presence: true
-  # validates :user,                presence: true 
 
   belongs_to :user
   # has_one :buy
-  has_one_attached :image #複数イメージを保存する？としたら違うメソッドに
+  has_one_attached :image
 
   
 end
