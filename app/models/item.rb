2e0a252dea79bcae1f5d28d7c06ba58407bb1817
isymkz44 <falot.323.66@yahoo.ne.jp>
2023-01-10 21:03:14 +0900

    itemモデル内　記述の整理
    モデル単体テスト確認ずみ

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,12 +6,23 @@ class Item < ApplicationRecord
   belongs_to :shipping_charge
   belongs_to :shipping_date
 
+  belongs_to :user
+  has_one_attached :image
+
   validates :category_id,
             :condition_id,
             :prefecture_id,
             :shipping_charge_id,
             :shipping_date_id,
-            :image,               presence: true
+            :image,              
+            :name,
+            :description,
+            :category_id,
+            :condition_id,
+            :shipping_date_id,    
+            :shipping_charge_id,  
+            :price,
+            :prefecture_id,       presence: true
   
   validates :category_id,
             :condition_id,
@@ -19,18 +30,10 @@ class Item < ApplicationRecord
             :shipping_charge_id,
             :shipping_date_id,    numericality: { other_than: 0,message: "can't be blank" } 
 
-  validates :name,                presence: true
-  validates :description,         presence: true
-  validates :price,               presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message:"is invalid" }
-  validates :category_id,         presence: true
-  validates :condition_id,        presence: true
-  validates :shipping_date_id,    presence: true
-  validates :shipping_charge_id,  presence: true
-  validates :prefecture_id,       presence: true
+  validates :price,               numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message:"is invalid" }
 
-  belongs_to :user
-  # has_one :buy
-  has_one_attached :image
+
+
 
   
 end
