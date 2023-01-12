616cdaf6656abce989c5cf4fe9abfbbbcf95d808
isymkz44 <falot.323.66@yahoo.ne.jp>
2023-01-12 21:03:24 +0900

    誤操作にて作成したディレクトリの削除→controllersディレクトリにまとめる

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,1 +1,21 @@
-class BuysController < ApplicationControll
+class BuysController < ApplicationController
+  def new
+    @buy_address = BuyAddress.new
+  end
+
+  def create
+    @buy_address = BuyAddress.new(buy_params)
+    if @buy_address.valid?
+       @buy_address.save
+       redirect_to root_path
+    else
+       render :new
+    end
+  end
+
+  private
+
+  def buy_params
+    params.require(:buy_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :buy_id).merge(user_id: current_user.id)
+  end
+end
