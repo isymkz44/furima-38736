8d733f282138989d3fdcebb5b4790a3762c6dad9
isymkz44 <falot.323.66@yahoo.ne.jp>
2023-01-12 21:04:27 +0900

    formオブジェクト（buy_address）の内容記述

--- app/models/buy_address.rb
+++ app/models/buy_address.rb
@@ -1,4 +1,19 @@
 class BuyAddress
-include ActiveModel::Model
-
-end
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id
+    with_options presence: true do
+      validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+      validates :city
+      validates :house_number
+      validates :building_name
+      validates :phone_number
+      validates :user_id
+      validates :item_id
+    end
+    validates  :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+   
+    def save
+      buy = Buy.create(item_id: item_id, user_id: user_id)
+      Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, buy_id: buy.id)
+    end
+  end
