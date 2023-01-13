f5b8bf608dd4fe07a618ee10a78bd9ab0d49441d
isymkz44 <falot.323.66@yahoo.ne.jp>
2023-01-13 10:34:27 +0900

    formオブジェクト内　バリデーションの追加

--- app/models/buy_address.rb
+++ app/models/buy_address.rb
@@ -1,7 +1,8 @@
 class BuyAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id
-    with_options presence: true do
+   
+  with_options presence: true do
       validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
       validates :city
       validates :house_number
