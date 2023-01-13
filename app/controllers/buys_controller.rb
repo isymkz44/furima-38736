0a85d4df58ad3ff68e5877d2eff5fbb2bb70cee9
isymkz44 <falot.323.66@yahoo.ne.jp>
2023-01-13 10:39:59 +0900

    item情報の取得をbefore_actionとしてnew,create共に実施。

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,6 +1,7 @@
 class BuysController < ApplicationController
+  before_action :item_set,only: [:new, :create]
+
   def new
-    @item = Item.find(params[:item_id])#←←”id"だと、orderモデルのparamsのIDを取得することになるため、itemのidが欲しい場合はこう記述
     @buy_address = BuyAddress.new
   end
 
@@ -19,4 +20,9 @@ class BuysController < ApplicationController
   def buy_params
     params.require(:buy_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :buy_id).merge(user_id: current_user.id)
   end
+
+  def item_set
+    @item = Item.find(params[:item_id])#←←”id"だと、orderモデルのparamsのIDを取得することになるため、itemのidが欲しい場合はこう記述
+  end
 end
+
