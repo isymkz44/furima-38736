e1118c757b50b127967a59c071e07b39a7b6dcc1
isymkz44 <falot.323.66@yahoo.ne.jp>
2023-01-13 16:40:23 +0900

    formオブジェクト記載変更、電話番号バリデーション変更

--- app/models/buy_address.rb
+++ app/models/buy_address.rb
@@ -1,13 +1,19 @@
 class BuyAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id
+  attr_accessor :postal_code, 
+                :prefecture_id, 
+                :city, 
+                :house_number, 
+                :building_name, 
+                :phone_number, 
+                :user_id, 
+                :item_id
    
   with_options presence: true do
       validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+      validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "is invalid. Exclude hyphen(-)"}
       validates :city
       validates :house_number
-      validates :building_name
-      validates :phone_number
       validates :user_id
       validates :item_id
     end
