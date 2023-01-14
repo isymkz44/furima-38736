43b7d83776c8267014734230e11bb3fa549030a3
isymkz44 <falot.323.66@yahoo.ne.jp>
2023-01-14 20:35:44 +0900

    buy_addressへのFactoryBotの導入

---
+++ spec/factories/buy_address.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :buy_address do
+    postal_code            { "123-4567" } 
+    prefecture_id          { 1 } 
+    city                   { "田中市" } 
+    house_number           { "田中町山田123" } 
+    building_name          { "田中ビル1" } 
+    phone_number           { "00000000000" } 
+  end
+end
