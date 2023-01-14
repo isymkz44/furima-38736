7b42ec2f3f4549fba6cdf90ca4fee339246afaeb
isymkz44 <falot.323.66@yahoo.ne.jp>
2023-01-14 20:33:35 +0900

    【fix】itemに関するFactoryBotの変更
    idの中に3が存在しない項目があるため

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,12 +3,12 @@ FactoryBot.define do
     association :user 
     name                    { "korehatesutodesu" }
     description             { "これはテスト出品です" }
-    price                   {20000}
-    category_id             { 3 }
-    condition_id            { 3 }
-    shipping_date_id        { 3 }
-    shipping_charge_id      { 3 }
-    prefecture_id           { 3 }   
+    price                   { 20000 }
+    category_id             { 1 }
+    condition_id            { 1 }
+    shipping_date_id        { 1 }
+    shipping_charge_id      { 1 }
+    prefecture_id           { 1 }   
     # image {Faker::Lorem.sentence}
     
     after(:build) do |item|
