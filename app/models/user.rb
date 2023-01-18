4ad7de18f21b478296e3edd03aca988407c63d2b
isymkz44 <falot.323.66@yahoo.ne.jp>
2023-01-18 16:29:18 +0900

    userバリデーションの整理

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,18 +8,25 @@ class User < ApplicationRecord
   VALID_FULL_WIDTH_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]/
   VALID_FULL_WIDTH_KANA_REGEX = /\A[ァ-ヶー－]+\z/
 
-  validates :password,
-            format: { with: VALID_PASSWORD_REGEX, message: 'is invalid. Include both half-width letters and half-width numbers' }
-  validates :first_name,          presence: true,
-                                  format: { with: VALID_FULL_WIDTH_REGEX, message: 'is invalid. Input full-width characters' }
-  validates :last_name,           presence: true,
-                                  format: { with: VALID_FULL_WIDTH_REGEX, message: 'is invalid. Input full-width characters' }
-  validates :first_name_reading,  presence: true,
-                                  format: { with: VALID_FULL_WIDTH_KANA_REGEX, message: 'is invalid. Input full-width katakana characters' }
-  validates :last_name_reading,   presence: true,
-                                  format: { with: VALID_FULL_WIDTH_KANA_REGEX, message: 'is invalid. Input full-width katakana characters' }
-  validates :nickname,            presence: true
-  validates :birthday,            presence: true
+  with_options presence: true do
+    validates :password,            format: { with: VALID_PASSWORD_REGEX }
+    validates :first_name
+    validates :last_name
+    validates :first_name_reading
+    validates :last_name_reading
+    validates :nickname            
+    validates :birthday
+  end         
+
+  with_options format: { with: VALID_FULL_WIDTH_REGEX } do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options format: { with: VALID_FULL_WIDTH_KANA_REGEX } do
+    validates :first_name_reading
+    validates :last_name_reading
+  end
 
   has_many :items, dependent: :destroy
   has_many :buys, dependent: :destroy
